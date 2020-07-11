b39729426e3a782a9a2ea2776f34b1921c49a1df
Marco Contreras <marco_contreras_cruz@hotmail.com>
2020-07-11 17:23:20 +0000

    initial project

---
+++ TestModels.py
@@ -0,0 +1,47 @@
+import Models as M
+import Global as G
+import numpy as np
+import tensorflow as tf
+import sys
+import time
+
+# CREATE AND LOAD THE MODEL
+if G.FUSION :
+    model = M.make_mobilenet_fusion( G.IMG_SHAPE )
+    name = 'models/mobilenet_fusion_weights_%d' % (G.EPOCHS)
+    model.load_weights( name )
+else :
+    model = M.make_mobilenet_model( G.IMG_SHAPE )
+    name = 'models/mobilenet_weights_%d' % (G.EPOCHS)
+    model.load_weights( name )
+
+#model.summary()
+
+path = "Dataset/test/"
+cmd = path + "data.txt"
+f = open( cmd, 'r' )
+for line in f :
+    row = line.split()
+    img = tf.io.read_file( path + row[ 0 ] )
+    img = tf.image.decode_jpeg( img, channels = 3 )
+    img = tf.image.convert_image_dtype( img, tf.float32 )
+    img = ( img - 0.5 ) / 0.5
+    img = tf.image.resize( img, [ G.IMG_SIZE, G.IMG_SIZE ] )
+    img = tf.reshape( img, ( 1, G.IMG_SIZE, G.IMG_SIZE, 3 ) )
+    imu = np.array( [ float( row[ 2 ] ), float( row[ 3 ] ), float( row[ 4 ] ),
+                      float( row[ 5 ] ), float( row[ 6 ] ), float( row[ 7 ] ) ] )
+    imu = np.reshape( imu, ( 1, 6 ) )
+    if G.FUSION :
+        # CHECK TIME
+        start_time = time.time()
+        a = model.predict( { "img_input": img, "imu_input": imu } )
+        time_sec = ( time.time() - start_time )
+    else :
+        # CHECK TIME
+        start_time = time.time()
+        a = model.predict( img )
+        time_sec = ( time.time() - start_time )
+    prediction = int( a[ 0 ][ 0 ] >= 0.5 )
+    print( prediction, time_sec )
+
+f.close()
---
+++ TrainingFusionModel.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+import Models as M
+import Global as G
+#import pathlib
+import os
+import numpy as np
+import random
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+# LOAD DATA (TEXT MODE)
+def loadData( path ) :
+    cmd = path + "data.txt"
+    f = open( cmd, 'r' )
+    names = [] # Create a list with the image names
+    label = [] # Create a list with the labels
+    imu_1 = []
+    imu_2 = []
+    imu_3 = []
+    imu_4 = []
+    imu_5 = []
+    imu_6 = []
+    for line in f :
+        row = line.split()
+        names.append( path + str( row[ 0 ] ) ) # Name of the image from the current folder
+        label.append( int( row[ 1 ] ) )
+        imu_1.append( float( row[ 2 ] ) )
+        imu_2.append( float( row[ 3 ] ) )
+        imu_3.append( float( row[ 4 ] ) )
+        imu_4.append( float( row[ 5 ] ) )
+        imu_5.append( float( row[ 6 ] ) )
+        imu_6.append( float( row[ 7 ] ) )
+    f.close()
+    full_list = list( zip( names, label, imu_1, imu_2, imu_3, imu_4, imu_5, imu_6 ) )
+    random.shuffle( full_list )
+    names, label, imu_1, imu_2, imu_3, imu_4, imu_5, imu_6 = zip( *full_list )
+    names = list( names )
+    label = list( label )
+    imu = list( zip( imu_1, imu_2, imu_3, imu_4, imu_5, imu_6 ) )
+    names_tensor = tf.convert_to_tensor( names, dtype = tf.string )
+    label_tensor = tf.convert_to_tensor( label, dtype = tf.int32 )
+    imu_tensor = tf.convert_to_tensor( imu, dtype = tf.float64 )
+    return names_tensor, label_tensor, imu_tensor
+
+train_names, train_label, train_imu = loadData( "Dataset/train/" )
+val_names, val_label, val_imu = loadData( "Dataset/validation/" )
+
+# GET DATA GIVEN A FILE PATH
+def get_image( file_path ) :
+    img = tf.io.read_file( file_path )                                          # Read an image from a path (here the path is a tensor)
+    img = tf.image.decode_jpeg( img, channels = 3 )
+    img = tf.image.convert_image_dtype( img, tf.float32 )                       # To convert to floats in the [0,1] range.
+    img = ( img - 0.5 ) / 0.5                                                     # To convert the image in the [-1,1] range.
+    img = tf.image.resize(img, [G.IMG_SIZE, G.IMG_SIZE])
+    return img
+
+def process_data_train( file_path ) :
+    img = get_image( file_path )
+    a = tf.strings.regex_full_match( train_names, file_path )                   # Search in the name column
+    index = tf.where( a )                                                       # Find the index
+    index = tf.reshape( index, () )                                             # Flat the tensor
+    return {'img_input': img, 'imu_input': train_imu[ index ]}, train_label[ index ]
+
+def process_data_val( file_path ) :
+    img = get_image( file_path )
+    a = tf.strings.regex_full_match( val_names, file_path )                   # Search in the name column
+    index = tf.where( a )                                                       # Find the index
+    index = tf.reshape( index, () )                                             # Flat the tensor
+    return {'img_input': img, 'imu_input': val_imu[ index ]}, val_label[ index ]
+
+def prepare_dataset( ds, shuffle_buffer_size = 1000, batch_size = 24 ) :
+    ds = ds.shuffle(buffer_size=shuffle_buffer_size)                            # Buffer size (element to be considered in the shuffle, True shuffle buffer_size >= data_size)
+    ds = ds.batch( batch_size )
+    ds = ds.prefetch( buffer_size=AUTOTUNE )                                    # Lets the dataset fetch batches in the background while the model is training.
+    return ds
+
+# CREATE THE DATASET
+train_dataset = tf.data.Dataset.from_tensor_slices( train_names )
+train_labeled_ds = train_dataset.map( process_data_train, num_parallel_calls = AUTOTUNE )
+train_ds = prepare_dataset( train_labeled_ds,
+                           shuffle_buffer_size = 1000,
+                           batch_size = G.BATCH_SIZE_TRAINING )
+
+val_dataset = tf.data.Dataset.from_tensor_slices( val_names )
+val_labeled_ds = val_dataset.map( process_data_val, num_parallel_calls = AUTOTUNE )
+val_ds = prepare_dataset( val_labeled_ds,
+                           shuffle_buffer_size = 1000,
+                           batch_size = G.BATCH_SIZE_VALIDATION )
+
+print( train_ds )
+print( val_ds ) 
+
+# CREATE THE MODEL
+model = M.make_mobilenet_fusion( G.IMG_SHAPE )
+
+# TRAINING THE MODEL
+model.fit(
+    train_ds,
+    #batch_size = G.BATCH_SIZE_TRAINING,
+    epochs = G.EPOCHS,
+    validation_data = val_ds,
+    #validation_batch_size = G.BATCH_SIZE_VALIDATION,
+    )
+
+# SAVE THE MODEL
+path = 'models/'
+cmd = 'mkdir ' + path
+os.system( cmd )
+filename = 'mobilenet_fusion_weights_%d' % (G.EPOCHS)
+model.save_weights( path + filename )
